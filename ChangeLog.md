73ee506d77569c48b1b959bb51c315853df773be
Paweł Nosal <pawel.nosal@ocado.com>
2020-07-23 11:46:56 +0200

    initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for test-issue
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib
+    ( someFunc
+    ) where
+
+-- >>> aaaa
+aaaa :: [String]
+aaaa = ["sdfasdf", "sdfasdfasdf"]
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
